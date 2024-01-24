cf325d5d4ed444be34ec7d9765d91633c76ded54
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-01-24 09:22:08 +0400

    Update content of about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+export const metadata = {
+  title: 'About Page WebTech Next.js',
+};
+
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <h2>About Lorem</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique
+        eum praesentium, numquam facere vitae consequatur quas debitis dolorem,
+        magni quaerat nostrum voluptatibus nesciunt cumque explicabo.
+        Exercitationem minima eius dicta!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique
+        eum praesentium, numquam facere vitae consequatur quas debitis dolorem,
+        magni quaerat nostrum voluptatibus nesciunt cumque explicabo.
+        Exercitationem minima eius dicta!
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
