8a9668a2b9e32cf93a1eb94b143227302adc406a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-01-24 10:05:32 +0400

    remove navigation from homepage as its already implemented in header component

--- app/page.js
+++ app/page.js
@@ -5,19 +5,6 @@ const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Home Page </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <Link href='/about'>About</Link>
-          </li>
-          <li>
-            <Link href='/about/team'>Team</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
