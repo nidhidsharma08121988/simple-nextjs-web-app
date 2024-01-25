540e9927bb825ca3dca3ad8b61356b6213e2664c
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-01-25 12:30:44 +0400

    Add Suspense Boundary in case data takes time to load, it will not stop your rest of the page from loading

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Repo from '@/app/components/Repo';
+import Link from 'next/link';
+import RepoDirs from '@/app/components/RepoDirs';
 
 const RepoPage = ({ params: { name } }) => {
   return (
-    <div className='card'>
-      <Repo name={name} />
+    <div>
+      <div className='container'>
+        <Link href='/code/repos' className='btn btn-back'>
+          Back to repositories
+        </Link>
+      </div>
+      <div className='card'>
+        <Suspense fallback={<div>Loading repo...</div>}>
+          <Repo name={name} />
+        </Suspense>
+        <Suspense fallback={<div>Loading directories...</div>}>
+          <RepoDirs name={name} />
+        </Suspense>
+      </div>
     </div>
   );
 };
