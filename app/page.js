b3f6ba291eb3cc8da94fa4dd4b07d260265bad1a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-01-25 14:08:21 +0400

    Update home page title

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
+import Products from './components/Products.jsx';
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to Home Page </h1>
+      <h1>Lets learn Nextjs</h1>
+      <Products />
     </div>
   );
 };
